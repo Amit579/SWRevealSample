be9ff7a1a01a3e7e889c2855acd6f3914f5df3a3
inveera technologies <amitsharma5july@gmail.com>
2016-06-22 11:39:36 +0530

    Initial Commit

---
+++ SWRevealSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SWRevealSample
+//
+//  Created by inveera technologies on 6/22/16.
+//  Copyright © 2016 inveera technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SWRevealSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SWRevealSample
+//
+//  Created by inveera technologies on 6/22/16.
+//  Copyright © 2016 inveera technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
